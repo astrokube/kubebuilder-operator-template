7a28885ad65642a01125dcf9b3688660f9572499
Iván Corrales <ivan.corrales.solera@gmail.com>
2023-04-12 07:39:42 +0200

    Define basic template

--- build/deps.mk
+++
@@ -1,6 +0,0 @@
-VENDOR_CMD	= $(GO_CMD) mod vendor
-
-.PHONY: deps
-deps: ## install dependencies
-	@echo "=== $(PROJECT_NAME) === [ deps             ]: Installing package dependencies required by the project..."
-	@$(VENDOR_CMD)
